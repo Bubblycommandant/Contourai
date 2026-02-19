10067443d24264d55881787ec4d61425bde62090
Bubblycommandant <aphad2@gmail.com>
2026-02-19 16:32:29 +0530

    Create atlas.ts

---
+++ lib/atlas.ts
@@ -0,0 +1,46 @@
+export const levelAtlas: Record<string, any> = {
+  IIa: {
+    cranial: "Skull base",
+    caudal: "Inferior border of hyoid bone",
+    medial: "Lateral border of internal carotid artery",
+    lateral: "Medial border of sternocleidomastoid (SCM)",
+    posterior: "Posterior border of SCM",
+    anterior: "Posterior edge of submandibular gland"
+  },
+
+  IIb: {
+    cranial: "Skull base",
+    caudal: "Inferior border of hyoid bone",
+    medial: "Spinal accessory nerve plane",
+    lateral: "Medial border of SCM",
+    posterior: "Posterior border of SCM",
+    anterior: "Posterior border of IIa"
+  },
+
+  III: {
+    cranial: "Inferior border of hyoid bone",
+    caudal: "Inferior border of cricoid cartilage",
+    medial: "Lateral border of carotid artery",
+    lateral: "Medial border of SCM",
+    posterior: "Posterior border of SCM",
+    anterior: "Anterior border of sternohyoid muscle"
+  },
+
+  IV: {
+    cranial: "Inferior border of cricoid cartilage",
+    caudal: "Clavicle / supraclavicular fossa",
+    medial: "Lateral border of common carotid artery",
+    lateral: "Medial border of SCM",
+    posterior: "Anterior border of trapezius",
+    anterior: "Posterior border of sternohyoid muscle"
+  },
+
+  RPN: {
+    cranial: "Skull base",
+    caudal: "Level of hyoid bone",
+    medial: "Posterior pharyngeal wall",
+    lateral: "Medial border of internal carotid artery",
+    posterior: "Prevertebral fascia",
+    anterior: "Pharyngeal constrictor muscles"
+  }
+};
